9ea07488ac8dc99a9167142e9940c3c1b730c915
Vyacheslav Savinkov <slava.savinkov1@mail.ru>
2025-04-26 16:20:24 +0500

    fix confing

--- config.py
+++ config.py
@@ -11,22 +11,11 @@ PASSWORD = config("PASSWORD")
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 DEBUG = False
-API_TOKEN = config("API_TOKEN")
-LANG_MESSAGES = config("LANG_MESSAGES")
-TELEGRAM_ADMIN_ID = int(config("TELEGRAM_ADMIN_ID"))
-REFERRER_PRIZE = float(config("REFERRER_PRIZE"))
 
-chats = config("CHATS", default="").strip()
 
-chat_links = chats.split(",") if chats else []
 # config.py
 
 RABBITMQ_URL = config("RABBITMQ_URL")
-TEST = config("TEST")
 TEST_MODE = config("TEST_MODE")
-CHANNEL_NOTIFICATION_ID = int(config("CHANNEL_NOTIFICATION_ID"))
-ID_ADMIN = str(config('ID_ADMIN'))
-ERROR_BOT_TOKEN = config('ERROR_BOT_TOKEN')
 JWT_SECRET_KEY = config('JWT_SECRET_KEY')
-EDIT_MESSAGE_BOT = str(config('EDIT_MESSAGE_BOT'))
 MODE = config('MODE')
